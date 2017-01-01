b4adb52cbe03aa5f31d245235ad93ad39f268cc4
ray247k <ray247k@gmail.com>
2017-01-01 21:36:22 +0800

    if練習

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,5 @@
-print("First Line")
-
-def fuc1():
-    print("World")
+def main():
+    print("Hello World")
 
 if __name__ == "__main__":
-    print("Hello")
-    fuc1()
-
-print("Last Line.")
+    main()    
---
+++ if.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+def main():
+    score = int(input("輸入分數"))
+    if score >= 90:
+        print("A")
+    elif score >= 80:
+        print("B")
+    elif  score>= 70:
+        print("C")
+    elif score >= 60:
+        print("D")
+    else:
+        print("F")
+if __name__ == "__main__":
+    print("成績分級")
+    main()
