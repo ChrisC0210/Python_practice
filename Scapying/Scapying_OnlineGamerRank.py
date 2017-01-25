a4e329e0bad5b51b695cfb8d20f9a541e3a09cc9
Qian247 <ray247k@gmail.com>
2017-01-25 16:12:09 +0800

    註解打錯字

--- Scapying/Scapying_OnlineGamerRank.py
+++ Scapying/Scapying_OnlineGamerRank.py
@@ -1,14 +1,11 @@
-# 用來爬巴哈姆特的OnlyGame排行
+# 巴哈姆特的OnlineGame排行
 import requests
 from bs4 import BeautifulSoup
-game_raking_html = requests.get(
-    'https://acg.gamer.com.tw/billboard.php?t=2&p=Android')
-game_raking_html.encoding = 'UTF-8'
-soup = BeautifulSoup(game_raking_html.text, "html.parser")
-# 抓取class_='ACG-mainbox1'內部class_='ACG-maintitle'中的<a>
-# 不過註解掉好像也沒差
-# soup.find(class_='ACG-mainbox1').find(class_='ACG-maintitle').find('a').string
-# 印出
+
+gameRank = requests.get(
+    'https://acg.gamer.com.tw/billboard.php?t=2&p=OLG', params=None)
+gameRank.encoding = 'UTF-8'
+soup = BeautifulSoup(gameRank.text, "html.parser")
 for game in soup.findAll(class_='ACG-mainbox1'):
     print(game.find(class_='ACG-mainumber').string + ' ' +
           game.find(class_='ACG-maintitle').find('a').string)
