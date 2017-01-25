0440fd964ba864bf8f35d3d3c351a3b20e76f354
Qian247 <ray247k@gmail.com>
2017-01-25 16:47:44 +0800

    增加提示
    加入提示訊息，讓使用者比較好了解這隻程式在做什麼

--- Scapying/Scapying_OnlineGamerRank.py
+++ Scapying/Scapying_OnlineGamerRank.py
@@ -1,6 +1,8 @@
 # 巴哈姆特的OnlineGame排行
 import requests
 from bs4 import BeautifulSoup
+import datetime
+print("巴哈姆特線上遊戲人氣排行\n")
 
 gameRank = requests.get(
     'https://acg.gamer.com.tw/billboard.php?t=2&p=OLG', params=None)
@@ -9,3 +11,4 @@ soup = BeautifulSoup(gameRank.text, "html.parser")
 for game in soup.findAll(class_='ACG-mainbox1'):
     print(game.find(class_='ACG-mainumber').string + ' ' +
           game.find(class_='ACG-maintitle').find('a').string)
+print("\n查詢日期" + datetime.datetime.now().date().strftime("%Y-%m-%d"))
